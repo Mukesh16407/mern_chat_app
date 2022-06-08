d2f1baf473119ef9fc3abbc66857bb6c35ac7dd4
Keshav-babu <keshavbabu742@gmail.com>
2022-06-08 22:49:44 +0530

    Creating API for accessChat FetchChat makeGroup renameGroup addOnGroup removeFromGropu

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const dotenv=require("dotenv")
 const connectDB = require("./config/db")
+const chatRoutes = require("./routes/chatRoutes");
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
@@ -25,6 +26,7 @@ app.get('/',(req,res)=>{
 // });
 
 app.use('/api/user',userRoutes)
+app.use('/api/chat',chatRoutes)
 
 const PORT=process.env.PORT||5000
 app.listen(PORT,console.log(`Running on port ${PORT}`))
